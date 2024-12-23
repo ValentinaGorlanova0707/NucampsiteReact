36f817a3cb7e8a401bb679c42abda26d65ec7c98
Valentina Gorlanova <vltny0707@gmail.com>
2024-12-23 00:16:37 -0500

    Homepage Display

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,6 +1,6 @@
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
-import { selectAllCampsites } from '../../components/campsitesSlice';
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = ({ setCampsiteId }) => {
     const campsites = selectAllCampsites();
