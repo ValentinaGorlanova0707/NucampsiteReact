1309c6e9652f127e0f1d874660bae73e8381439b
Valentina Gorlanova <vltny0707@gmail.com>
2025-01-05 12:29:17 -0500

    ContactForm

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,16 @@
+import ContactForm from '../components/ContactForm';
+import SubHeader from '../components/SubHeader';
+import { Container, Col } from 'reactstrap';
+
 const ContactPage = () => {
-    return <div>ContactPage</div>;
+    return (
+        <Container>
+            <SubHeader current='Send Us Your Feedback'  detail={false} />
+            <Col md='10'>
+                    <ContactForm />
+            </Col>
+    </Container>
+    );
 };
 
 export default ContactPage;
